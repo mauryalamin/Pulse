22e730a91b7de91a398ea4b41816d71d9221a448
Maury Alamin <malamin@comcast.net>
2025-05-07 14:38:51 -0500

    Initial Commit

---
+++ Pulse/PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseApp.swift
+//  Pulse
+//
+//  Created by Maury Alamin on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
